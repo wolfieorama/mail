7c56208fce016df98a80fdf57abdf41af8f0f888
wolfieorama <wachirawamunyi@gmail.com>
2016-09-08 00:37:08 +0300

    added showEmail action in folder/index controller

---
+++ app/controllers/folder/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember'
+
+export default Ember.Controller.extend({
+  actions: {
+    showEmail(email){
+      this.transitionToRoute('folder.mail', email);
+      // this is the controller context ref transitionToRoute
+    }
+  }
+});
