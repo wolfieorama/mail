b966589c1dbeb0b182dafb16178af307d0fa17d0
wolfieorama <wachirawamunyi@gmail.com>
2016-09-12 12:24:20 +0300

    added read && unread class + afterModel hook

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,7 +8,8 @@ export default Ember.Route.extend({
       to: 'dev@me.com',
       subject: 'Opportunity',
       body: 'Dear {{firstName}}, ...',
-      sentAt: new Date()//calling new on date
+      sentAt: new Date(),//calling new on date
+      readDate: null
     },
     {
       id: 2,
@@ -16,7 +17,8 @@ export default Ember.Route.extend({
       to: 'riko@me.com',
       subject: 'Foo',
       body: 'Dear {{firstName}}, ...',
-      sentAt: new Date()//calling new on date
+      sentAt: new Date(),//calling new on date
+      readDate: null
     }];
   }
 });
--- app/routes/folder/mail.js
+++ app/routes/folder/mail.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
+const { set } = Ember;
+
 export default Ember.Route.extend({
   model({ mailId }){
     return this.modelFor('application')[mailId-1];
   },
   serialize(model){
     return { mailId: model.id };
+  },
+  afterModel(model){
+    set(model, 'readDate', new Date());
   }
 });
