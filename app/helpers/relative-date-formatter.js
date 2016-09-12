7230008ad70c16b46ad8cea98cba0f2316aa373e
wolfieorama <wachirawamunyi@gmail.com>
2016-09-13 00:38:12 +0300

    added relative date in mail

--- app/helpers/relative-date-formatter.js
+++ app/helpers/relative-date-formatter.js
@@ -1,12 +1,28 @@
 import Ember from 'ember';
 import moment from 'moment';
 
-export function relativeDateFormatter([date]/*, hash*/) {
-  return `${formatTime(date)} (1 hour ago)`;
+export function relativeDateFormatter([date], { relative }) {
+  const old = moment().diff(date, 'days');
+  const time = old ? formatDate(date) : formatTime(date);
+  if (relative){
+    return `${time} (${relativeTime(date)})`;
+  }
+  else {
+    return time;
+  }
 }
 
 function formatTime(date){
   return moment(date).format('h:mm A');
+}
+
+function formatDate(date){
+  return moment(date).format('MMM D');
+}
+
+function relativeTime(date){
+  const result = moment(date).fromNow();// to return something like an hour ago etc
+  return result.replace(/^(an|a) /, '1 ');
 }
 
 export default Ember.Helper.helper(relativeDateFormatter);
--- tests/unit/helpers/relative-date-formatter-test.js
+++ tests/unit/helpers/relative-date-formatter-test.js
@@ -2,12 +2,24 @@ import { relativeDateFormatter } from 'mail/helpers/relative-date-formatter';
 import { module, test } from 'qunit';
 import moment from 'moment';
 
-module('Unit | Helper | relative date formatter');
+module('Unit | Helper | relative date formatter', {
+  afterEach(){ // resets timekeeper incase we are simulating time elsewhere
+    timekeeper.reset();
+  }
+});
+
+const date = moment("07/27/2016 1:00 PM", "MM/DD/YYYY h:mm:ss A").toDate(); //moment lib to work with dates
 
 test('show relative time for date\'s that are less than 24 hours old', function(assert){
-  const date = moment("07/27/2016 1:00 PM", "MM/DD/YYYY h:mm:ss A").toDate(); //moment lib to work with dates
   const laterDate = moment(date).add(1, 'hour').toDate();
   timekeeper.freeze(laterDate); //timekeeper lib to freeze time
-  const actual = relativeDateFormatter([date]);
+  const actual = relativeDateFormatter([date], { relative: true });
   assert.equal(actual, '1:00 PM (1 hour ago)');
+});
+
+test('show just date when relative is false', function(assert){
+  const laterDate = moment(date).add(2, 'day').toDate();
+  timekeeper.freeze(laterDate); //timekeeper lib to freeze time
+  const actual = relativeDateFormatter([date], { relative: false });
+  assert.equal(actual, 'Jul 27');
 });
