6ed792efd8ea83606b67c43c8f45258005fce7c2
wolfieorama <wachirawamunyi@gmail.com>
2016-09-14 00:41:12 +0300

    declared fixtures outside the model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,31 +1,32 @@
 import Ember from 'ember';
 
+const fixtures = [{
+  id: 1,
+  from: 'recruiter@startup.com',
+  to: 'dev@me.com',
+  subject: 'Opportunity',
+  body: 'Dear {{firstName}}, ...',
+  sentAt: new Date(),//calling new on date
+  readDate: null,
+  trashedDate: null
+},
+{
+  id: 2,
+  from: 'riko@startup.com',
+  to: 'riko@me.com',
+  subject: 'Foo',
+  body: 'Dear {{firstName}}, ...',
+  sentAt: new Date(),//calling new on date
+  readDate: null,
+  trashedDate: null
+}];
+
+const { get } = Ember;
+
 export default Ember.Route.extend({
   model(){
-    const fixtures = [{
-      id: 1,
-      from: 'recruiter@startup.com',
-      to: 'dev@me.com',
-      subject: 'Opportunity',
-      body: 'Dear {{firstName}}, ...',
-      sentAt: new Date(),//calling new on date
-      readDate: null,
-      trashedDate: null
-    },
-    {
-      id: 2,
-      from: 'riko@startup.com',
-      to: 'riko@me.com',
-      subject: 'Foo',
-      body: 'Dear {{firstName}}, ...',
-      sentAt: new Date(),//calling new on date
-      readDate: null,
-      trashedDate: null
-    }];
-
-    const { get } = Ember;
-    return fixtures.filter(function(email) {
-      return !get(email, 'trashedDate');
+    return fixtures.filter(function(item) {
+      return !get(item, 'trashedDate');
     });
   }
 });
